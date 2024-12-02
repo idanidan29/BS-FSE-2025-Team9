09cb4f559b09bdd469610126502d44e7de92b7cf
Idan Levi <29idan29@gmail.com>
2024-12-02 13:13:50 +0200

    login page

---
+++ client/src/app/components/NavBar.jsx
@@ -0,0 +1,142 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { FaRegFilePowerpoint } from "react-icons/fa";
+import { FaRegUser } from "react-icons/fa";
+import { VscAccount } from "react-icons/vsc";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaFileDownload } from "react-icons/fa";
+import { FaChartSimple } from "react-icons/fa6";
+import { FaSignOutAlt } from "react-icons/fa";
+
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const sideList = [
+        {
+            icon: <VscAccount className="text-2xl mr-2" />,
+            title: "Sign In",
+            path: "/Sign",
+        },
+        {
+            icon: <FaFileDownload  className="text-2xl mr-2" />,
+            title: "Exel File",
+            path: "/Exel",
+        },
+        {
+            icon: <FaChartSimple  className="text-2xl mr-2" />,
+            title: "Data",
+            path: "/Data",
+        },
+        {
+            icon: <FaSignOutAlt   className="text-2xl mr-2" />,
+            title: "Sing Out",
+            path: "/Out",
+        }
+    ];
+
+    const navList = [
+        {
+            icon: <FaRegFilePowerpoint className="text-2xl mr-2" />,
+            title: "Parking File",
+            path: "/",
+        },
+        {
+            icon: <FaRegUser className="text-2xl mr-2" />,
+            title: "Sign In",
+            path: "/Sign",
+        },
+    ];
+
+    const handleDrawer = () => {
+        setIsOpen(!isOpen);
+    };
+
+    useEffect(() => {
+        const handleEscKeyPress = (e) => {
+            if (e.keyCode === 27 && isOpen) {
+                setIsOpen(false);
+            }
+        };
+
+        if (isOpen) {
+            document.body.style.setProperty("overflow", "hidden");
+        } else {
+            document.body.style.removeProperty("overflow");
+        }
+
+        document.addEventListener("keydown", handleEscKeyPress);
+
+        return () => {
+            document.removeEventListener("keydown", handleEscKeyPress);
+        };
+    }, [isOpen]);
+
+    return (
+        <nav className="sticky top-0 flex w-full items-center justify-between px-6 h-16 bg-gray-800 text-gray-200 border-b border-gray-700 z-10">
+            <div className="flex items-center">
+                <button className="mr-2" aria-label="Open Menu" onClick={handleDrawer}>
+                    <GiHamburgerMenu className="text-3xl text-gray-300" />
+                </button>
+
+                <img
+                    src="/images/SCE_logo.png"
+                    alt="Logo"
+                    className="h-auto w-24"
+                />
+            </div>
+
+            <div className="flex items-center">
+                <div className="hidden md:flex md:justify-between md:bg-transparent">
+                    {navList.map(({ icon, title, path }, index) => {
+                        return (
+                            <Link key={index} href={path} passHref>
+                                <button
+                                    title={title}
+                                    className="flex items-center p-3 font-medium mr-2 text-center bg-gray-700 text-gray-200 rounded hover:bg-gray-600 hover:text-white focus:outline-none focus:bg-gray-600 focus:text-white"
+                                >
+                                    <span>{icon}</span>
+                                    <span>{title}</span>
+                                </button>
+                            </Link>
+                        );
+                    })}
+                </div>
+            </div>
+
+            {isOpen && (
+                <div className="z-10 fixed inset-0 transition-opacity">
+                    <div
+                        onClick={() => setIsOpen(false)}
+                        className="absolute inset-0 bg-black opacity-50"
+                        tabIndex="0"
+                    ></div>
+                </div>
+            )}
+
+            <aside
+                className={`transform top-0 left-0 w-64 bg-gray-900 fixed h-full overflow-auto ease-in-out transition-all duration-300 z-30 ${isOpen ? "translate-x-0" : "-translate-x-full"
+                    }`}
+            >
+                <span className="flex w-full items-center p-4 border-b border-gray-700">
+                    <img
+                        src="/images/SCE_logo.png"
+                        alt="Logo"
+                        className="h-auto w-32 mx-auto"
+                    />
+                </span>
+                {sideList.map(({ icon, title, path }, index) => {
+                    return (
+                        <Link key={index} href={path} passHref>
+                            <span className="flex items-center p-4 text-gray-300 hover:bg-gray-700 hover:text-white transition-colors duration-200">
+                                <span className="mr-2">{icon}</span> <span>{title}</span>
+                            </span>
+                        </Link>
+                    );
+                })}
+            </aside>
+        </nav>
+    );
+};
+
+export default Navbar;
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,101 +1,43 @@
 import Image from "next/image";
+import Navbar from "./components/NavBar";
+import Layout from "./components/layout";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>44464363463463.</li>
-        </ol>
+    <Layout>
+        <div className="flex items-center justify-center h-screen bg-[#fff]">
+            <div className="bg-[#15172b] rounded-2xl box-border h-[450px] p-5 w-[520px]">
+                <div className="text-[#eee] font-sans text-4xl font-semibold mt-8 text-center">Sing In</div>
+              
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+                <div className="relative w-full mt-10">
+                    <input
+                        id="user_name"
+                        className="bg-[#303245] rounded-xl border-0 box-border text-[#eee] text-lg h-[50px] outline-none px-5 pt-1 w-full"
+                        type="text"
+                        placeholder="User Name"
+                    />
+                    <div className="bg-[#15172b] h-5 w-[76px] absolute top-[-20px] left-5 rounded-lg transition-transform transform"></div>
+                </div>
+
+                <div className="relative w-full mt-7">
+                    <input
+                        id="password"
+                        className="bg-[#303245] rounded-xl border-0 box-border text-[#eee] text-lg h-[50px] outline-none px-5 pt-1 w-full"
+                        type="password"
+                        placeholder="Password"
+                    />
+                    <div className="bg-[#15172b] h-5 w-[76px] absolute top-[-20px] left-5 rounded-lg transition-transform transform"></div>
+                </div>
+
+                <button className="bg-[#08d] rounded-xl border-0 text-[#eee] text-lg h-[50px] mt-9 w-full hover:bg-[#06b]">
+                    Submit
+                </button>
+                <button className="bg-[rgb(209,213,219) rounded-xl border-0 text-[#eee] text-lg h-[50px] mt-9 w-full hover:rgb(48 50 69)">
+                    I Dont Have A User
+                </button>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+        </Layout>
+    );
 }
