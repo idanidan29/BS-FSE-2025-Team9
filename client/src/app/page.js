6ceb1a0d6eccafd18fcc397df3ac58fdf3cb206a
Idan Levi <29idan29@gmail.com>
2024-12-03 11:48:33 +0200

    adding logo

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -4,16 +4,26 @@ import Layout from "./components/layout";
 
 export default function Home() {
     return (
+        <div className="flex flex-col items-center justify-center h-screen bg-[#fff]">
+            <div className="mb-10"> 
+                <Image
+                    src="/SCE_logo.png" 
+                    alt="Logo"
+                    width={350}  
+                    height={350} 
+                    className="h-auto w-auto"  
+                />
+            </div>
 
-        <div className="flex items-center justify-center h-screen bg-[#fff]">
-            <div className="bg-[#fff] rounded-2xl box-border h-[450px] p-5 w-[520px]">
-                <div className="text-[#eee] font-sans text-4xl font-semibold mt-8 text-center text-green-500">Sing In</div>
-
+            <div className="bg-[#fff] rounded-2xl box-border h-[450px] w-[520px]">
+                <div className="text-[#eee] font-sans text-4xl font-semibold mt-8 text-center text-green-500">
+                    Sign In
+                </div>
 
                 <div className="relative w-full mt-10">
                     <input
                         id="user_name"
-                        className="bg-[#fff] h-[70px] rounded-xl border border-green-500 box-border text-bg-black text-lg h-[50px] outline-none px-5 pt-1 w-full"
+                        className="bg-[#fff] h-[70px] rounded-xl border border-green-500 box-border text-bg-black text-lg outline-none px-5 pt-1 w-full"
                         type="text"
                         placeholder="User Name"
                     />
@@ -21,13 +31,11 @@ export default function Home() {
 
                 <div className="relative w-full mt-7">
                     <input
-                        id="user_name"
-                        className="bg-[#fff] h-[70px] rounded-xl border border-green-500 box-border text-bg-black text-lg h-[50px] outline-none px-5 pt-1 w-full"
-                        type="Password"
+                        id="password"
+                        className="bg-[#fff] h-[70px] rounded-xl border border-green-500 box-border text-bg-black text-lg outline-none px-5 pt-1 w-full"
+                        type="password"
                         placeholder="Password"
                     />
-
-
                 </div>
 
                 <button className="bg-green-500 rounded-full border-0 text-[#eee] text-lg h-[50px] mt-9 w-full hover:bg-green-600">
@@ -36,9 +44,7 @@ export default function Home() {
                 <button className="bg-[#fff] text-green-500 rounded-full border border-green-500 text-lg h-[50px] mt-9 w-full hover:bg-green-500 hover:text-white">
                     I Don’t Have A User
                 </button>
-
             </div>
         </div>
-
     );
 }
