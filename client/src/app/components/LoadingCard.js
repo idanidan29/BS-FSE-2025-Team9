f01f61acde069ec10b98799218c5df658d185d88
Idan Levi <29idan29@gmail.com>
2025-01-05 13:49:17 +0200

    adding pulse to loading animation

--- client/src/app/components/LoadingCard.js
+++ client/src/app/components/LoadingCard.js
@@ -3,7 +3,7 @@ import React from 'react';
 // components/LoadingCard.js
 export default function LoadingCard() {
   return (
-    
+
     <div className="min-h-screen w-full bg-gradient-to-br from-green-300 via-teal-200 to-cyan-300">
       {/* Skeleton Navbar */}
       <div className="bg-green-500 text-white p-4 shadow-md w-full animate-pulse">
@@ -13,17 +13,17 @@ export default function LoadingCard() {
       </div>
 
       <div className="min-h-screen w-full bg-gradient-to-br from-green-300 via-teal-200 to-cyan-300 p-5">
-  {/* Skeleton Content Card */}
-  <div className="bg-white rounded-2xl box-border min-h-[600px] p-5 w-full sm:w-[520px] mx-auto ">
-    <div className="h-8 bg-gray-300 rounded mb-4"></div>
-    <div className="h-10 bg-gray-300 rounded mb-4"></div>
-    <div className="h-10 bg-gray-300 rounded mb-4"></div>
-    <div className="h-10 bg-gray-300 rounded mb-4"></div>
-    <div className="h-10 bg-gray-300 rounded mb-4"></div>
-    <div className="h-10 bg-gray-300 rounded mb-4"></div>
-    <div className="h-12 bg-gray-300 rounded mb-4"></div>
-  </div>
-</div>
+        {/* Skeleton Content Card */}
+        <div className="bg-white rounded-2xl box-border min-h-[600px] p-5 w-full sm:w-[520px] mx-auto animate-pulse ">
+          <div className="h-8 bg-gray-300 rounded mb-4"></div>
+          <div className="h-10 bg-gray-300 rounded mb-4"></div>
+          <div className="h-10 bg-gray-300 rounded mb-4"></div>
+          <div className="h-10 bg-gray-300 rounded mb-4"></div>
+          <div className="h-10 bg-gray-300 rounded mb-4"></div>
+          <div className="h-10 bg-gray-300 rounded mb-4"></div>
+          <div className="h-12 bg-gray-300 rounded mb-4"></div>
+        </div>
+      </div>
 
     </div>
 
