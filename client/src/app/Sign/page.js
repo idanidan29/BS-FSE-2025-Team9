886a701d60c78d1fbee4e70878df394537a4b4d4
Idan Levi <29idan29@gmail.com>
2025-01-06 13:26:47 +0200

    small production bug fix

--- client/src/app/Sign/page.js
+++ client/src/app/Sign/page.js
@@ -1,7 +1,8 @@
 "use client";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 export default function Page() {
     const router = useRouter();
