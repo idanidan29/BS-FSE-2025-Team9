9d4dfd1d4a4e927b4355e2e8df183318afa59a65
Idan Levi <29idan29@gmail.com>
2025-01-09 11:30:09 +0200

    more adjusting

--- client/src/app/[ParkingFile]/search/page.js
+++ client/src/app/[ParkingFile]/search/page.js
@@ -184,7 +184,7 @@ export default function Page() {
 
                                                                 {/* Dropdown Menu */}
                                                                 {dropdownOpen === user.student_id && (
-                                                                    <div className="absolute right-0 mt-2 w-40 bg-white border border-gray-300 rounded-lg shadow-md z-10">
+                                                                    <div className="absolute right-[100px] top-[-40px] w-30 bg-white border border-gray-300 rounded-lg shadow-md z-10">
                                                                         <button
                                                                             onClick={() => handleDelete(user.student_id)}
                                                                             className="block px-4 py-2 text-sm text-red-600 hover:bg-gray-100"
