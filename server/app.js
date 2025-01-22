64b67423e48467b747ac19182d48ea33ae81dca2
Idan Levi <29idan29@gmail.com>
2025-01-22 12:47:57 +0200

    fixing deployment bug

--- server/app.js
+++ server/app.js
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ limit: '7mb', extended: true }));
 
 
 // Connect to MongoDB
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(mongoUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('Error connecting to MongoDB:', error));
 
